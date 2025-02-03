206c35ba5556d41570a9caf94d68b4f00bae29ea
Evan <ec8865994@gmail.com>
2025-02-03 14:57:04 +0900

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    swcMinify: true,
-    /**
-     * Ref: https://lucide.dev/guide/packages/lucide-react
-     * ...dynamic import, be careful to integrate this into the app...
-     * ...it makes the speed of compiling extremely slow...
-     * ...so try to avoid this while the app is still being developed...
-     */
-    // transpilePackages: ['lucide-react']
+  output: 'export',
+  swcMinify: true,
+  /**
+   * Ref: https://lucide.dev/guide/packages/lucide-react
+   * ...dynamic import, be careful to integrate this into the app...
+   * ...it makes the speed of compiling extremely slow...
+   * ...so try to avoid this while the app is still being developed...
+   */
+  // transpilePackages: ['lucide-react']
 };
 
 module.exports = nextConfig;
