fafc7d9936b0100d5ce61f3e3f04a49f8f1e70ba
EvanChen350 <ec8865994@gmail.com>
2025-02-05 15:36:06 +0900

    Update scene.tsx

--- src/app/[lng]/(default)/_components/scene.tsx
+++ src/app/[lng]/(default)/_components/scene.tsx
@@ -24,7 +24,7 @@ export default function Scene() {
       <div className='mx-auto w-full max-w-8xl min-h-screen py-20 relative'>
         {/* Scene 01 */}
         <div
-          className='md:max-h-[1250px] max-h-[650px] grid grid-rows-12 grid-cols-12 px-5 md:px-10 text-gray-900'
+          className='md:max-h-[1250px] max-h-[650px] grid grid-rows-12 md:grid-rows-9 grid-cols-12 px-5 md:px-10 text-gray-900'
           ref={(el) => (scene01Refs.current.trigger = el)}
         >
           {/* <span
@@ -34,12 +34,12 @@ export default function Scene() {
             001
           </span> */}
           <h2
-            className='md:row-start-5 row-start-3 row-span-2 col-start-7 col-span-5 font-bodoniMT text-[22px] md:text-[56px] font-bold text-left leading-[1.2] tracking-wide z-10'
+            className='md:row-start-2 row-start-3 row-span-2 col-start-7 col-span-5 font-bodoniMT text-[22px] md:text-[56px] font-bold text-left leading-[1.2] tracking-wide z-10'
             ref={(el) => (scene01Refs.current.title = el)}
             dangerouslySetInnerHTML={{ __html: t('concentrateTitle') }}
           />
           <div
-            className='row-start-6 row-span-5 col-start-1 col-span-7 opacity-80'
+            className='row-start-6 md:row-start-3 row-span-5 col-start-1 col-span-7 opacity-80'
             ref={(el) => (scene01Refs.current.pic = el)}
           >
             <Image
@@ -52,20 +52,20 @@ export default function Scene() {
             />
           </div>
           <p
-            className='row-start-9 row-span-1 col-start-9 md:col-start-8 col-span-4 max-w-[317px] font-normal text-sm leading-tight justify-self-center self-end mb-6'
+            className='row-start-9 md:row-start-7 row-span-1 col-start-9 md:col-start-8 col-span-4 max-w-[317px] font-normal text-sm leading-tight justify-self-center self-end mb-6'
             ref={(el) => (scene01Refs.current.desc = el)}
           >
             {t('sence-p1')}
           </p>
           <div
-            className='md:row-start-9 row-start-5 row-span-3 col-start-7 col-span-5 max-w-[600px] max-h-[300px] bg-shape -z-10'
+            className='md:row-start-6 row-start-5 row-span-3 col-start-7 col-span-5 max-w-[600px] max-h-[300px] bg-shape -z-10'
             ref={(el) => (scene01Refs.current.shape = el)}
           />
         </div>
 
         {/* Scene 02 */}
         <div
-          className='md:max-h-[1250px] max-h-[650px] grid grid-rows-12 grid-cols-12 px-5 md:px-10 text-gray-900 mt-20'
+          className='md:max-h-[1250px] max-h-[650px] grid grid-rows-12 md:grid-rows-9 grid-cols-12 px-5 md:px-10 text-gray-900 mt-20'
           ref={(el) => (scene02Refs.current.trigger = el)}
         >
           {/* <span
@@ -75,12 +75,12 @@ export default function Scene() {
             002
           </span> */}
           <h2
-            className='md:row-start-6 row-start-2 row-span-2 col-start-2 col-span-6 font-bodoniMT text-[22px] md:text-[56px] font-bold text-left leading-[1.2] tracking-wide self-center ml-2 z-10'
+            className='md:row-start-2 row-start-2 row-span-2 col-start-2 col-span-6 font-bodoniMT text-[22px] md:text-[56px] font-bold text-left leading-[1.2] tracking-wide self-center ml-2 z-10'
             ref={(el) => (scene02Refs.current.title = el)}
             dangerouslySetInnerHTML={{ __html: t('quietTitle') }}
           />
           <div
-            className='md:row-start-4 row-start-2 row-span-7 col-start-6 col-span-6 place-self-end opacity-80'
+            className='md:row-start-2 row-start-2 row-span-7 col-start-6 col-span-6 place-self-end opacity-80'
             ref={(el) => (scene02Refs.current.pic = el)}
           >
             <Image
@@ -92,20 +92,20 @@ export default function Scene() {
             />
           </div>
           <p
-            className='md:row-start-8 row-start-4 row-span-1 col-start-1 md:col-start-3 md:col-span-3 col-span-4 max-w-[317px] font-normal text-sm leading-tight justify-self-center self-start mt-6'
+            className='md:row-start-5 row-start-4 row-span-1 col-start-1 md:col-start-3 md:col-span-3 col-span-4 max-w-[317px] font-normal text-sm leading-tight justify-self-center self-start mt-6'
             ref={(el) => (scene02Refs.current.desc = el)}
           >
             {t('sence-p2')}
           </p>
           <div
-            className='md:row-start-9 row-start-4 row-span-4 col-start-4 col-span-4 max-w-[390px] aspect-square bg-shape justify-self-end -z-10'
+            className='md:row-start-4 row-start-4 row-span-4 col-start-4 col-span-4 max-w-[390px] aspect-square bg-shape justify-self-end -z-10'
             ref={(el) => (scene02Refs.current.shape = el)}
           />
         </div>
 
         {/* Scene 03 */}
         <div
-          className='md:max-h-[1250px] max-h-[650px] grid grid-rows-12 grid-cols-12 px-5 md:px-10 text-gray-900 mt-20'
+          className='md:max-h-[1250px] max-h-[650px] grid grid-rows-12 md:grid-rows-9 grid-cols-12 px-5 md:px-10 text-gray-900 mt-20'
           ref={(el) => (scene03Refs.current.trigger = el)}
         >
           {/* <span
@@ -115,12 +115,12 @@ export default function Scene() {
             003
           </span> */}
           <h2
-            className='md:row-start-5 row-start-2 row-span-1 col-start-5 col-span-7 font-bodoniMT text-[22px] md:text-[56px] font-bold text-left leading-[1.2] tracking-wide'
+            className='md:row-start-2 row-start-2 row-span-1 col-start-5 col-span-7 font-bodoniMT text-[22px] md:text-[56px] font-bold text-left leading-[1.2] tracking-wide'
             ref={(el) => (scene03Refs.current.title = el)}
             dangerouslySetInnerHTML={{ __html: t('privacyTitle') }}
           />
           <div
-            className='md:row-start-7 row-start-6 row-span-5 col-start-1 col-span-8 w-fit h-fit justify-self-center opacity-80 relative before:absolute before:w-full before:h-full before:top-16 before:left-16 before:bg-shape before:-z-10'
+            className='md:row-start-4 row-start-6 row-span-5 col-start-1 col-span-8 w-fit h-fit justify-self-center opacity-80 relative before:absolute before:w-full before:h-full before:top-16 before:left-16 before:bg-shape before:-z-10'
             ref={(el) => (scene03Refs.current.pic = el)}
           >
             <Image
